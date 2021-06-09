2ade0f63f7bbc70bb101c035f1caea768313157f
NeCKrose <gael.41370@orange.fr>
2021-06-10 00:16:15 +0200

    FIRST VERSION OF THE WELCOME SCREEN
    This version of the welcome screen includes the background image, a button which will be used to start the quizz, one timer, one counter for the score and one for the highscore. There is no back at the moment, so it's only a single page with no interactions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'movizz-webapp';
+  score = 0;
+  highScore = 0;
+  timeLeft = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onStartQuizz(): void {
+
+  }
+
+  onQuizzYesAnswer(): void {
+    this.score += 1;
+  }
+
+  onQuizzNoAnswer(): void {
+
+  }
 }
