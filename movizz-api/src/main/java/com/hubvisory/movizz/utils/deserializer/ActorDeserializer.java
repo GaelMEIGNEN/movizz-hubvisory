dfe4fca9061947246e9371fd61913ff92c540a25
NeCKrose <gael.41370@orange.fr>
2021-06-12 19:53:27 +0200

    CREATING/UPDATING DESERIALIZER
    To be able to use the Json from TMDB API, we have to read its content and create object out of it. It means creating a Deserializer for every type of object expected in return of TMDB API call.
    In this case, we create a deserializer for an Actor and we modify the Movie one to add a method used in QuizzController to call deserialization

---
+++ movizz-api/src/main/java/com/hubvisory/movizz/utils/deserializer/ActorDeserializer.java
@@ -0,0 +1,95 @@
+package com.hubvisory.movizz.utils.deserializer;
+
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.hubvisory.movizz.beans.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class ActorDeserializer extends StdDeserializer<Person[]> {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public ActorDeserializer(){
+        this(null);
+    }
+
+    public ActorDeserializer(Class<?> c){
+        super(c);
+    }
+
+    /**
+     *  Deserialize the content of a Json file in an array of Movies
+     *
+     * @param jsonParser
+     * @param deserializationContext
+     * @return A table of Movies
+     * @throws IOException
+     */
+    @Override
+    public Person[] deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+
+        ObjectCodec codec = jsonParser.getCodec();
+        TreeNode treeNode = codec.readTree(jsonParser);
+        try {
+            TreeNode results = treeNode.get("cast");
+
+            Person[] actors = new Person[results.size()];
+            int i = 0;
+            int j = 0;
+            while (i<results.size()) {
+                if (results.get(i).get("known_for_department").toString().contains("Acting")){
+
+                    Person actor = new Person();
+
+                    actor.setAdult(Boolean.getBoolean(results.get(i).get("adult").toString()));
+                    actor.setGender(Integer.parseInt(results.get(i).get("gender").toString()));
+                    actor.setId(Long.parseLong(results.get(i).get("id").toString()));
+                    actor.setKnown_for_department(results.get(i).get("known_for_department").toString().replaceAll("[\"\\].*[\\\"]]", ""));
+                    actor.setName(results.get(i).get("name").toString().replaceAll("[\"\\].*[\\\"]]", ""));
+                    actor.setOriginal_name(results.get(i).get("original_name").toString().replaceAll("[\"\\].*[\\\"]]", ""));
+                    actor.setPopularity(Double.parseDouble(results.get(i).get("popularity").toString()));
+                    actor.setProfile_path(results.get(i).get("profile_path").toString().substring(2,results.get(i).get("profile_path").toString().length()-1));
+
+                    actors[j] = actor;
+                    j++;
+                }
+                i++;
+            }
+            return actors;
+        } catch (Exception e) {
+            logger.error("Error in ActorDeserializer::deserialize :",e);
+        }
+        return null;
+    }
+
+    /**
+     * This method use the class MovieDeserializer to deserialize the Json content from TMDB API.
+     *
+     * @param apiResults The Json content from TMDB API as a String
+     * @return A list of deserialized movies
+     */
+    public Person[] actorsDeserialize(String apiResults) {
+        Person[] actors = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            SimpleModule module = new SimpleModule("ActorDeserializer", new Version(1, 0, 0, null, null, null));
+            module.addDeserializer(Person[].class, this);
+            mapper.registerModule(module);
+            actors = mapper.readValue(apiResults, Person[].class);
+        } catch (JsonMappingException e) {
+            logger.error("Error in ActorDeserializer::actorsDeserialize :", e);
+        } catch (JsonProcessingException e2) {
+            logger.error("Error in ActorDeserializer::actorsDeserialize :", e2);
+        }
+
+        return actors;
+    }
+
+}
--- movizz-api/src/main/java/com/hubvisory/movizz/utils/deserializer/MovieDeserializer.java
+++ movizz-api/src/main/java/com/hubvisory/movizz/utils/deserializer/MovieDeserializer.java
@@ -1,63 +1,91 @@
 package com.hubvisory.movizz.utils.deserializer;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.ObjectCodec;
-import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.hubvisory.movizz.beans.Movie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class MovieDeserializer extends StdDeserializer<Movie[]> {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public MovieDeserializer(){
         this(null);
     }
 
-    private MovieDeserializer(Class<?> c){
+    public MovieDeserializer(Class<?> c){
         super(c);
     }
 
+    /**
+     *  Deserialize the content of a Json file in an array of Movies
+     *
+     * @param jsonParser
+     * @param deserializationContext
+     * @return A table of Movies
+     * @throws IOException
+     */
     @Override
     public Movie[] deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
 
         ObjectCodec codec = jsonParser.getCodec();
         TreeNode treeNode = codec.readTree(jsonParser);
-        System.out.println("test4545");
         try {
             TreeNode results = treeNode.get("results");
-
             Movie[] movies = new Movie[results.size()];
-            for (int i = 0; i<=results.size(); i++){
+            for (int i = 0; i<results.size(); i++){
                 Movie movie = new Movie();
 
-                movie.setAdult(results.get(i).get("adult").toString());
-                movie.setBackdrop_path(results.get(i).get("backdrop_path").toString());
+                movie.setAdult(Boolean.getBoolean(results.get(i).get("adult").toString()));
+                movie.setBackdrop_path(results.get(i).get("backdrop_path").toString().replaceAll("[\"\\].*[\\\"]]", ""));
                 movie.setId(Long.parseLong(results.get(i).get("id").toString()));
-                movie.setOriginal_language(results.get(i).get("original_language").toString());
-                movie.setOriginal_title(results.get(i).get("original_title").toString());
-                movie.setOverview(results.get(i).get("overview").toString());
+                movie.setOriginal_language(results.get(i).get("original_language").toString().replaceAll("[\"\\].*[\\\"]]", ""));
+                movie.setOriginal_title(results.get(i).get("original_title").toString().replaceAll("[\"\\].*[\\\"]]", ""));
+                movie.setOverview(results.get(i).get("overview").toString().replaceAll("[\"\\].*[\\\"]]", ""));
                 movie.setPopularity(Double.parseDouble(results.get(i).get("popularity").toString()));
-                movie.setPoster_path(results.get(i).get("poster_path").toString());
-                movie.setTitle(results.get(i).get("title").toString());
+                movie.setPoster_path(results.get(i).get("poster_path").toString().replaceAll("[\"\\].*[\\\"]]", ""));
+                movie.setTitle(results.get(i).get("title").toString().replaceAll("[\"\\].*[\\\"]]", ""));
                 movie.setVideo(Boolean.getBoolean(results.get(i).get("video").toString()));
-                movie.setVote_average(Integer.parseInt(results.get(i).get("vote_average").toString()));
+                movie.setVote_average(Double.parseDouble(results.get(i).get("vote_average").toString()));
                 movie.setVote_count(Integer.parseInt(results.get(i).get("vote_count").toString()));
+
                 movies[i] = movie;
             }
-            System.out.println(results.get(1));
-
-            System.out.println("test8080");
             return movies;
-
         } catch (Exception e) {
-            System.out.println("test8083");
-            System.out.println(e.getMessage());
+            logger.error("Error in MovieDeserializer::deserialize :", e);
         }
-        System.out.println("test8084");
         return null;
+    }
+
+    /**
+     * This method use the class MovieDeserializer to deserialize the Json content from TMDB API.
+     *
+     * @param apiResults The Json content from TMDB API as a String
+     * @return A list of deserialized movies
+     */
+    public Movie[] moviesDeserialize(String apiResults) {
+        Movie[] movies = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            SimpleModule module = new SimpleModule("MovieDeserializer", new Version(1, 0, 0, null, null, null));
+            module.addDeserializer(Movie[].class, this);
+            mapper.registerModule(module);
+            movies = mapper.readValue(apiResults, Movie[].class);
+        } catch (JsonMappingException e) {
+            logger.error("Error in MovieDeserializer::moviesDeserialize :", e);
+        } catch (JsonProcessingException e2) {
+            logger.error("Error in MovieDeserializer::moviesDeserialize :", e2);
+        }
+
+        return movies;
     }
 
 
