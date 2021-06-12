648083c44d11d665eecb0b9ce980230c3a078cc8
NeCKrose <gael.41370@orange.fr>
2021-06-12 19:27:53 +0200

    ADDING LOG4J AND UPDATING CONFIG FILES
    Added log4j
    Using the class ApplicationProperties to find values of parameters inside application.properties

---
+++ movizz-api/src/main/java/com/hubvisory/movizz/config/ApplicationProperties.java
@@ -0,0 +1,69 @@
+package com.hubvisory.movizz.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("tmdb.api")
+public class ApplicationProperties {
+
+    private String url;
+
+    private String key;
+
+    private String get_movie_from_id;
+
+    private String get_person;
+
+    private String discover_movie;
+
+    private String get_credits_from_movie;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getGet_movie_from_id() {
+        return get_movie_from_id;
+    }
+
+    public String getGet_person() {
+        return get_person;
+    }
+
+    public String getDiscover_movie() {
+        return discover_movie;
+    }
+
+    public String getGet_credits_from_movie() {
+        return get_credits_from_movie;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setGet_movie_from_id(String get_movie_from_id) {
+        this.get_movie_from_id = get_movie_from_id;
+    }
+
+    public void setGet_person(String get_person) {
+        this.get_person = get_person;
+    }
+
+    public void setDiscover_movie(String discover_movie) {
+        this.discover_movie = discover_movie;
+    }
+
+    public void setGet_credits_from_movie(String get_credits_from_movie) {
+        this.get_credits_from_movie = get_credits_from_movie;
+    }
+}
