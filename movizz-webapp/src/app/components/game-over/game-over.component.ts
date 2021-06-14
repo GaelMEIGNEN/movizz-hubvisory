58d33ad2ba07f2aba253a060c7881163b4548409
NeCKrose <gael.41370@orange.fr>
2021-06-14 20:40:40 +0200

    GAME OVER PAGE
    Adding game over page which is called when the countdown of the quizz reaches 0. It simply displays the score, highscore and a retry button to start another quizz.

---
+++ movizz-webapp/src/app/components/game-over/game-over.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {QuizzService} from "../../services/quizz.service";
+import {Router} from "@angular/router";
+import {CookieService} from "ngx-cookie-service";
+
+@Component({
+  selector: 'app-game-over',
+  templateUrl: './game-over.component.html',
+  styleUrls: ['./game-over.component.css']
+})
+export class GameOverComponent implements OnInit {
+
+  score = 0;
+  highScore = 0;
+
+  constructor(private quizzService: QuizzService, private cookieService: CookieService, private router: Router) {
+    this.score = Number(this.cookieService.get("score"));
+    this.highScore = Number(this.cookieService.get("highScore"));
+  }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * Starts the quizz by getting a question and beginning the countdown
+   */
+  onStartQuizz(): void {
+    this.quizzService.getQuestionFromServer();
+    this.cookieService.set("score", "0");
+    this.router.navigate(['quizz'])
+  }
+
+}
